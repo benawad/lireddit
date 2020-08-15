49dbaec5c41dcda0117e2ba4d75723fea1cb3ab9
Ben Awad <benawad97@gmail.com>
2020-08-15 12:25:35 -0500

    change-vote

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -18,6 +18,7 @@ import { MyContext } from "../types";
 import { isAuth } from "../middleware/isAuth";
 import { getConnection } from "typeorm";
 import { Updoot } from "../entities/Updoot";
+import { tmpdir } from "os";
 
 @InputType()
 class PostInput {
@@ -52,25 +53,52 @@ export class PostResolver {
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
-    // await Updoot.insert({
-    //   userId,
-    //   postId,
-    //   value: realValue,
-    // });
-    await getConnection().query(
-      `
-    START TRANSACTION;
 
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
+
+    // the user has voted on the post before
+    // and they are changing their vote
+    if (updoot && updoot.value !== realValue) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+    update updoot
+    set value = $1
+    where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
+
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2
+        `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      // has never voted before
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
     insert into updoot ("userId", "postId", value)
-    values (${userId},${postId},${realValue});
+    values ($1, $2, $3)
+        `,
+          [userId, postId, realValue]
+        );
 
+        await tm.query(
+          `
     update post
-    set points = points + ${realValue}
-    where id = ${postId};
-
-    COMMIT;
-    `
-    );
+    set points = points + $1
+    where id = $2
+      `,
+          [realValue, postId]
+        );
+      });
+    }
     return true;
   }
 
