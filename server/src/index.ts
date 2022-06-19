0825e7b5a9ec577ba4ab17eecbe51ad2703837d8
Toby-28 <tobymarshal2802@gmail.com>
2022-06-19 12:21:54 +0500

    s

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,48 +1,48 @@
-import "reflect-metadata";
-import "dotenv-safe/config";
-import { __prod__, COOKIE_NAME } from "./constants";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-import { UserResolver } from "./resolvers/user";
-import Redis from "ioredis";
-import session from "express-session";
-import connectRedis from "connect-redis";
-import cors from "cors";
-import { createConnection } from "typeorm";
-import { Post } from "./entities/Post";
-import { User } from "./entities/User";
-import path from "path";
-import { Updoot } from "./entities/Updoot";
-import { createUserLoader } from "./utils/createUserLoader";
-import { createUpdootLoader } from "./utils/createUpdootLoader";
+import 'reflect-metadata'
+import 'dotenv-safe/config'
+import { __prod__, COOKIE_NAME } from './constants'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
+import { UserResolver } from './resolvers/user'
+import Redis from 'ioredis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import cors from 'cors'
+import { createConnection } from 'typeorm'
+import { Post } from './entities/Post'
+import { User } from './entities/User'
+import path from 'path'
+import { Updoot } from './entities/Updoot'
+import { createUserLoader } from './utils/createUserLoader'
+import { createUpdootLoader } from './utils/createUpdootLoader'
 
 const main = async () => {
   const conn = await createConnection({
-    type: "postgres",
+    type: 'postgres',
     url: process.env.DATABASE_URL,
     logging: true,
     // synchronize: true,
-    migrations: [path.join(__dirname, "./migrations/*")],
+    migrations: [path.join(__dirname, './migrations/*')],
     entities: [Post, User, Updoot],
-  });
-  // await conn.runMigrations();
+  })
+  await conn.runMigrations()
 
   // await Post.delete({});
 
-  const app = express();
+  const app = express()
 
-  const RedisStore = connectRedis(session);
-  const redis = new Redis(process.env.REDIS_URL);
-  app.set("trust proxy", 1);
+  const RedisStore = connectRedis(session)
+  const redis = new Redis(process.env.REDIS_URL)
+  app.set('trust proxy', 1)
   app.use(
     cors({
       origin: process.env.CORS_ORIGIN,
       credentials: true,
     })
-  );
+  )
   app.use(
     session({
       name: COOKIE_NAME,
@@ -53,15 +53,15 @@ const main = async () => {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
         httpOnly: true,
-        sameSite: "lax", // csrf
+        sameSite: 'lax', // csrf
         secure: __prod__, // cookie only works in https
-        domain: __prod__ ? ".codeponder.com" : undefined,
+        domain: __prod__ ? '.codeponder.com' : undefined,
       },
       saveUninitialized: false,
-      secret: process.env.SESSION_SECRET,
+      secret: process.env.SESSION_SECRET!,
       resave: false,
     })
-  );
+  )
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
@@ -75,18 +75,18 @@ const main = async () => {
       userLoader: createUserLoader(),
       updootLoader: createUpdootLoader(),
     }),
-  });
+  })
 
   apolloServer.applyMiddleware({
     app,
     cors: false,
-  });
+  })
 
-  app.listen(parseInt(process.env.PORT), () => {
-    console.log("server started on localhost:4000");
-  });
-};
+  app.listen(parseInt(process.env.PORT!), () => {
+    console.log('server started on localhost:4000')
+  })
+}
 
 main().catch((err) => {
-  console.error(err);
-});
+  console.error(err)
+})
